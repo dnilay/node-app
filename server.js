c14e514b2cd3eb343c1df094fa37ad688ac91e8c
Nilay <mailnilaydas@gmail.com>
2023-03-29 20:00:19 +0530

    5th commit

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Node App - version-1!!</h1> <br\>');
-  res.send('<h2 style="color:green;">Welcome to ECS CodeDeploy</h2> <br\>');
+  res.send('<h1 style="color:green;">Node App - version-2!!</h1> <br\>');
+
 });
 
 app.listen(PORT, HOST);
